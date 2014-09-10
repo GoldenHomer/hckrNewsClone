3096e9df14ac308811a3cd3460201884af7b41ca
Jose Contreras <jocontre@joselcontreras.com>
2014-09-09 22:51:08 -0700

    Just experimentation for now

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 angular.module('fauxNews', [])
   .controller('MainCtrl', ['$scope', function($scope){
     $scope.test = "Taste like malk";
-    $scope.posts = ["post1", "post2", "post3", "post4", "post5"];
+    $scope.posts = [
+      {title: "post1", upvotes: 5},
+      {title: "post2", upvotes: 2},
+      {title: "post3", upvotes: 15},
+      {title: "post4", upvotes: 9},
+      {title: "post5", upvotes: 4}
+    ];
   }])
